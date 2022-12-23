b85420ac1b8825ed520e274daad1e6d24485822e
Roman <roman76547810@gmail.com>
2022-12-23 19:42:38 +0600

    added IInjectable

---
+++ SpaceBattle.Lib/StopMoveCommand/IInjectable.cs
@@ -0,0 +1,6 @@
+namespace SpaceBattle.Lib;
+
+public interface IInjectable
+{
+    public void Inject(ICommand obj);
+}
--- SpaceBattle.Lib/StopMoveCommand/StopMoveCommand.cs
+++ SpaceBattle.Lib/StopMoveCommand/StopMoveCommand.cs
@@ -5,6 +5,15 @@ public class StopMoveCommande: ICommand
 {
     private IMoveCommandStopable obj;
     
-    public 
+    public StopMoveCommande(IMoveCommandStopable obj)
+    {
+        this.obj = obj;
+    }
+
+    public void Execute()
+    {
+        obj.properties.ToList().ForEach(p => IoC.Resolve<ICommand>("SpaceBattle.Game.SetProperty", obj.uobject, p).Execute());
+        
+    }
 
 }
